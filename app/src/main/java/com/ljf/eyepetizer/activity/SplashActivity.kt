e148557bd1f4932b4f69f5694a8cb05756831516
mr.lin <8524560>
2017-12-24 20:36:34 +0800

    播放闪屏视频

--- app/src/main/java/com/ljf/eyepetizer/activity/SplashActivity.kt
+++ app/src/main/java/com/ljf/eyepetizer/activity/SplashActivity.kt
@@ -1,10 +1,13 @@
 package com.ljf.eyepetizer.activity
 
-import android.content.Intent
 import android.os.Bundle
 import com.ljf.eyepetizer.BaseActivity
 import com.ljf.eyepetizer.R
+import com.ljf.eyepetizer.utils.CacheUtils
+import com.ljf.eyepetizer.utils.SpUtils
+import com.ljf.eyepetizer.views.ijkplayer.SimpleIjkPlayerListener
 import kotlinx.android.synthetic.main.activity_splash.*
+import tv.danmaku.ijk.media.player.IMediaPlayer
 
 /**
  * Created by mr.lin on 2017/12/13.
@@ -15,15 +18,37 @@ class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+    }
 
+    override fun onResume() {
+        super.onResume()
         goMain()
     }
 
     private fun goMain() {
-        textView.postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }, 1000)
+        if (isFirst()) {
+            showSplash()
+            SpUtils.setInt(SpUtils.KEY_FIRST_START, 1)
+        } else {
+        }
+    }
+
+    //首次打开
+    private fun isFirst(): Boolean {
+        return true
+//        return SpUtils.getInt(SpUtils.KEY_FIRST_START, 0) == 0
+    }
+
+    private fun showSplash() {
+
+//        TODO("onResume 后崩溃")
+        videoview_ijk.setVideoViewIjkListener(object : SimpleIjkPlayerListener() {
+            override fun onPrepared(p0: IMediaPlayer) {
+                p0.start()
+            }
+        })
+        videoview_ijk.setLooping(true)
+        videoview_ijk.setDataSource(CacheUtils.getFileDir() + "landing.mp4")
     }
 
 }
